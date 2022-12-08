075f15e4d17151c1af590f4dd09bc7c4e5a5105a
kyung-doo <hohomkd@naver.com>
2022-12-08 13:06:05 +0900

    no message

--- dev-car/js/mainVisual.js
+++ dev-car/js/mainVisual.js
@@ -1,18 +1,145 @@
 $(function (){
 
-   function cubicBezier (t, p0, p1, p2, p3) {
-      var cX = 3 * (p1.x - p0.x),
-          bX = 3 * (p2.x - p1.x) - cX,
-          aX = p3.x - p0.x - cX - bX;
-    
-      var cY = 3 * (p1.y - p0.y),
-          bY = 3 * (p2.y - p1.y) - cY,
-          aY = p3.y - p0.y - cY - bY;
-    
-      var x = (aX * Math.pow(t, 3)) + (bX * Math.pow(t, 2)) + (cX * t) + p0.x;
-      var y = (aY * Math.pow(t, 3)) + (bY * Math.pow(t, 2)) + (cY * t) + p0.y;
-    
-      return {x: x, y: y};
+   
+   var isMobile = false;
+   
+   if('ontouchstart' in window){
+      isMobile = true;
    }
+
+   var touchstart = "mousedown";
+   var touchmove = "mousemove";
+   var touchend = "mouseup";
+
+   if( isMobile ) {
+      touchstart = "touchstart";
+      touchmove = "touchmove";
+      touchend = "touchend";
+   }
+
+
+   var owner = this;
+   var button = $(".main-visual .control button");
+   var line = $(".main-visual .control .line");
+   var isTouch = false;
+   var timer = null;
+   var xPercent = 0;
+   var time = 0;
+   var activeIndex = 0;
+   var prevIndex = 0;
+   
+   var carTitles = [
+      "TORRES", 
+      "REXTON (7인승)", 
+      "REXTON (5인승)", 
+      "REXTON SPORTS<br />KHAN", 
+      "REXTON SPORTS", 
+      "KORANDO", 
+      "TIVOLI AIR", 
+      "TIVOLI"
+   ];
+
+   button.on(touchstart, $.proxy(onTouchStart, owner));
+   $(window).on(touchmove, $.proxy(onTouchMove, owner));
+   $(window).on(touchend, $.proxy(onTouchEnd, owner));
+   $(window).on("touchcancel", $.proxy(onTouchEnd, owner));  
+
+   var swiper = new Swiper(".main-visual .swiper", {
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 0,
+      loop: true
+    });
+
+
+   function onTouchStart(e){
+      isTouch = true;
+      timer = 0;
+      timer = setInterval($.proxy(onUpdate, owner), 1000/60);
+      e.preventDefault();
+      e.stopPropagation();
+   }
+
+   function onTouchMove( e ) {
+      if(isTouch) {
+         var pageX = e.originalEvent.changedTouches ? e.originalEvent.changedTouches[0].clientX : e.pageX;
+         var targetX = pageX - ($(window).width()/2);
+         var maxMove = (line.width()/2) - ($(window).width() < 767 ? 50 : 100);
+         
+         if(targetX <= -maxMove) targetX = -maxMove;
+         if(targetX >= maxMove) targetX = maxMove;
+         if(targetX <= -($(window).width()/2)+50) targetX = -($(window).width()/2)+ ($(window).width() < 767 ? 30 : 50);
+         if(targetX >= ($(window).width()/2)-50) targetX = ($(window).width()/2)- ($(window).width() < 767 ? 30 : 50);
+         var ro = $(window).width() > 1200 ? 68 : $(window).width() > 767 ? 60 : 75;
+         var rotation = toRadian(ro * Math.abs(targetX/maxMove));
+         gsap.to(button, 0.2, {
+            x: targetX, 
+            rotation:toDegree(targetX > 0 ? -rotation: rotation)/7, 
+            ease:Cubic.easeOut,
+            onUpdate: function () {
+               gsap.set(button, { y: (line.height()/2-20) * Math.cos(rotation)-($(window).width() <= 767 ? 30 : 85)});
+            }
+         });
+         xPercent = targetX/maxMove;
+      }
+   }
+
+   function onTouchEnd( e ) {
+      isTouch = false;
+      gsap.to(button, 0.6, {x: 0, rotation:0, ease:Cubic.easeOut, onUpdate: function () {
+         var ro = $(window).width() > 1200 ? 68 : $(window).width() > 767 ? 60 : 75;
+         var maxMove = (line.width()/2) - ($(window).width() < 767 ? 50 : 100);
+         var targetX = parseInt(button[0]._gsap.x);
+         var rotation = toRadian(ro * Math.abs(targetX/maxMove));
+         gsap.set(button, { y: (line.height()/2-20) * Math.cos(rotation)-($(window).width() <= 767 ? 30 : 85)});
+      }});
+      clearInterval(timer);
+      xPercent = 0;
+   }
+
+   function onUpdate() {
+      if(time >= 60) {
+         if(xPercent > 0.3)         changeCar('next');
+         else if(xPercent < -0.3)   changeCar('prev');
+         time = 0;
+      }
+      time++;
+   }
+
+   function changeCar( arrow ) {
+      if(arrow === "next") {
+         activeIndex++;
+         swiper.slideNext();
+         if(activeIndex === carTitles.length) {
+            activeIndex = 0;
+         }
+      } else {
+         activeIndex--;
+         swiper.slidePrev();
+         if(activeIndex === -1) {
+            activeIndex = carTitles.length-1;
+         }
+      }
+      
+      $(".main-visual .bg").css("z-index", 0);
+      $(".main-visual .bg.car"+(prevIndex+1)).css("z-index", 1);
+      $(".main-visual .bg.car"+(activeIndex+1)).css("z-index", 2);
+      gsap.set($(".main-visual .bg.car"+(activeIndex+1)), {opacity: 0});
+      gsap.to($(".main-visual .bg.car"+(activeIndex+1)), 0.4, {opacity: 1});
+      $(".main-visual .top-info h3").html(carTitles[activeIndex]);
+      $(".main-visual .swiper-slide").removeClass("active").eq(activeIndex).addClass("active");
+      
+      prevIndex = activeIndex;
+      console.log('changeCar', arrow, activeIndex)
+   }
+
+   function toRadian( d ) {
+      return Math.PI * d/180;
+   }
+
+   function toDegree( r ) {
+      return 180/Math.PI * r;
+   }
+   
    
 });
